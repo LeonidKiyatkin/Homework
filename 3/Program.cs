02dd29fd8182f50c95650746e57caa52f0b4a59b
Leonid <leonid@mail.ru>
2022-12-03 22:50:43 +0300

    3-rd task

---
+++ 3/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает число и выдает четное ли оно
+
+//Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+
+}
+else
+{
+    Console.Write("Число нечетное");
+}
