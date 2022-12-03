236ccadc7186d45a322ce457679cd921ab721aab
Leonid <leonid@mail.ru>
2022-12-03 21:55:25 +0300

    first task

---
+++ 1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear ();
+Console.WriteLine ("Введите первое число (А):");
+int number1 = int.Parse(Console.ReadLine ());
+Console.WriteLine ("Введите второе число (B):");
+int number2 = int.Parse(Console.ReadLine ());
+
+if (number1 > number2)
+{
+    Console.WriteLine ("А больше В");
+    }
+    else
+    if (number1 < number2)
+    {
+        Console.WriteLine ("А меньше В");
+            }
+    else
+    {
+        Console.WriteLine ("А равно В");
+    }
