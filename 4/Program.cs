8193ad2a902f2e02c8e3688da7494c47ca71cd73
Leonid <leonid@mail.ru>
2022-12-03 23:21:02 +0300

    4-th task

---
+++ 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// принимает число и показывает все четные от 1 до этого числа.
+
+Console.Clear();
+Console.WriteLine("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= N)
+{
+   if (i % 2 == 0)
+   {
+       Console.WriteLine($"{i}");
+    }
+    i++;
+}
+Console.WriteLine();
+
+
+
