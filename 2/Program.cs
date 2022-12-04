81a6797768a514089d083d74f1f4f08550b9418f
Leonid <leonid@mail.ru>
2022-12-04 13:51:48 +0300

    2-nd task again

--- 2/Program.cs
+++ 2/Program.cs
@@ -1,3 +1,20 @@
 ﻿//принимает три числа и выдет максимальное
 
+Console.WriteLine ("ВВедите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("ВВедите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("ВВедите третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
 
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+
+Console.WriteLine($"Максимальное число {max}");
