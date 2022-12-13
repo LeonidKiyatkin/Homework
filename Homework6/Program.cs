97dfda2b1d5516d0d570c6b3c68d5c8da0ce2c0c
Leonid <leonid@mail.ru>
2022-12-13 13:46:42 +0300

    hw6

--- Homework6/Program.cs
+++ Homework6/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите количество элементов массива: ");
+int ElementsCount = int.Parse(Console.ReadLine());
+    int [] matr  = new int [ElementsCount];
+    for (int i = 0; i < matr.Length; i ++)
+    {
+          Console.Write("Введите число: ");
+          matr [i] = int.Parse(Console.ReadLine());
+    }
+  
+
+int count = 0;
+int Number (int [] matr)
+{
+for (int i = 0; i < matr.Length; i++)
+{
+    if (matr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+return count;
+}
+
+Number(matr);
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Пересечение в точке: ({x};{y}) ");
